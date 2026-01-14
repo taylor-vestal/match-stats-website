07a41a913a2bd88cd9cd1b7fef725d87f4ec77f7
fractal161 <628snapdragon@gmail.com>
2026-01-13 19:52:55 -0800

    devtools

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs({ devtools: true })],
 });
