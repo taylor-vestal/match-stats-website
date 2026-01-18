34bcbe63ff3841ac778ae98a4895c5b756fc7172
fractal161 <628snapdragon@gmail.com>
2026-01-17 17:51:57 -0800

    add player type

---
+++ src/lib/player.ts
@@ -0,0 +1,16 @@
+import { statsDb } from "./stats-db";
+
+class Player {
+  private id: string;
+
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  getName() {
+    return statsDb.playerNames().get(this.id);
+  }
+  getAvatarUrl(): string {
+    return `/img/avatar/${this.id}.png`;
+  }
+}
